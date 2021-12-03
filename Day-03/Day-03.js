173aa51e63f0b90f412c8e92ae12458ac98be3a8
Rohit Bhat <rohitwritescode@gmail.com>
2021-12-03 13:16:15 -0800

    Completed Part 1 of Day 3

---
+++ Day-03/Day-03.js
@@ -0,0 +1,35 @@
+//Read input and organize into nested arrays:
+const fs = require("fs");
+const input = fs.readFileSync("./input.txt", "utf-8");
+const binArrays = input.split("\r\n").map(binary => binary.split(''));
+
+//Calculate Gamma and Epsilon Rate
+function calculateGammaAndEpsilonRate(binArrays) {
+
+    //Calculate 2D array with bits sorted by index (1st bit of all inputs sorted into an array in index 1, 2nd bit of all inputs sorted into an array in index 2 etc):
+    let finalbinArray2d = new Array(binArrays[0].length).fill(0).map(() => new Array(binArrays.length).fill(0));
+    binArrays.forEach((binArray, arrayIndex) => {
+        binArray.forEach((bit, bitIndex) => {
+            finalbinArray2d[bitIndex][arrayIndex] = bit;
+        });
+    });
+
+    //Calculate Gamma Rate:
+    const gammaRateArray = finalbinArray2d.map(bitArray => {
+        if (bitArray.filter(bit => bit==0).length > bitArray.filter(bit => bit==1).length) {
+            return 0;
+        } else {
+            return 1;
+        }
+    });
+
+    //Calculate Epsison Rate:
+    const epsilonRateArray = gammaRateArray.map(bit => bit = 1- bit);
+
+    //Calculate Power Consumption:
+    powerConsumption = parseInt(gammaRateArray.join(''), 2) * parseInt(epsilonRateArray.join(''), 2);
+    console.log(`The Power Consumption is: ${powerConsumption}`)
+}
+
+calculateGammaAndEpsilonRate(binArrays);
+
