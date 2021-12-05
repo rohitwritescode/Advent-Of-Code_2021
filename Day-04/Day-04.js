e8547a73aa38d769344a150cc114c0d3296dd0d5
rohitwritescode <rohitwritescode@gmail.com>
2021-12-05 15:31:58 -0800

    Refactored code for puzzle 1 to add functions for better performance

--- Day-04/Day-04.js
+++ Day-04/Day-04.js
@@ -1,4 +1,4 @@
-//Read input and organize into nested arrays:
+//Read input and organize into required arrays:
 const fs = require("fs");
 const input = fs.readFileSync("./input.txt", "utf-8");
 const arrays = input.split('\r\n\r\n').map((element, idx) => {
@@ -6,49 +6,51 @@ const arrays = input.split('\r\n\r\n').map((element, idx) => {
     else return element
 });
 
-function drawNumber(array) {
-    return array.shift();
-}
+// Puzzle 1: Calculate score for first game that is won:
+function puzzle1CalculateScore(inputArrays) {
+    const numbersToDraw = inputArrays.shift().split(',').map(el => parseInt(el));
+    const bingoBoards = inputArrays.map(array => array.map(element => element.toString().split(' ').filter(cleanedEl => cleanedEl !== '').map(num => parseInt(num))));
+    const transposedBingoBoards = bingoBoards.map(board => transpose(board));
 
-function transpose(matrix) {
-    return matrix[0].map((col, i) => matrix.map(row => row[i]));
-}
-
-function lookupBingoBoards(bingoBoardsArray, numbersToLookup) {
-    let checker = (arr, target) => target.every(v => arr.includes(v));
-    const checkedBingoBoardsArray = bingoBoardsArray.map(board => board.map(row => checker(numbersToLookup,row)).some(element => element === true));
-    return checkedBingoBoardsArray;
-}
-
-const numbersToDraw = arrays.shift().split(',').map(el => parseInt(el));
-const bingoBoards = arrays.map(array => array.map(element => element.toString().split(' ').filter(cleanedEl => cleanedEl !== '').map(num => parseInt(num))));
-const transposedBingoBoards = bingoBoards.map(board => transpose(board));
-
-// console.log('Total numbers to draw', numbersToDraw.length);
-// console.log('Transposed Boards', transposedBingoBoards[0]);
-
-let drawnNumbers =[];
-let gameOver = false;
-while(gameOver !== true) {
-    drawnNumbers.push(drawNumber(numbersToDraw));
-
-    if(lookupBingoBoards(bingoBoards, drawnNumbers).some(board => board === true)) {
-        gameOver = true;
-        console.log('Game has been won!')
-        const winningIndex = lookupBingoBoards(bingoBoards, drawnNumbers).findIndex(element => element === true);
-        const scoreArray = bingoBoards[winningIndex].map(row => row.filter(el => !drawnNumbers.includes(el))).flat();
-        const score = scoreArray.reduce((accumulator, val) => accumulator + val);
-        console.log(score * drawnNumbers[drawnNumbers.length - 1]);
-    } else if(lookupBingoBoards(transposedBingoBoards, drawnNumbers).some(board => board === true)){
-        gameOver = true;
-        console.log('Game has been won!')
-        const winningIndex = lookupBingoBoards(transposedBingoBoards, drawnNumbers).findIndex(element => element === true);
-        const scoreArray = transposedBingoBoards[winningIndex].map(row => row.filter(el => !drawnNumbers.includes(el))).flat();
-        const score = scoreArray.reduce((accumulator, val) => accumulator + val);
-        console.log(score * drawnNumbers[drawnNumbers.length - 1]);
+    // Draw number from available array of numbers:
+    function drawNumber(array) {
+        return array.shift();
     }
+
+    // Transpose board for easy comparison using array methods:
+    function transpose(matrix) {
+        return matrix[0].map((col, i) => matrix.map(row => row[i]));
+    }
+
+    // Check array of boards with numbers that have been drawn so far:
+    function lookupBingoBoards(bingoBoardsArray, numbersToLookup) {
+        let checker = (arr, target) => target.every(v => arr.includes(v));
+        const checkedBingoBoardsArray = bingoBoardsArray.map(board => board.map(row => checker(numbersToLookup,row)).some(element => element === true));
+        return checkedBingoBoardsArray;
+    }
+
+    // Begin drawing numbers one by one, and verify drawn numbers with all boards:
+    let drawnNumbers =[];
+    let gameOver = false;
+    while(gameOver !== true) {
+        drawnNumbers.push(drawNumber(numbersToDraw));
+
+        // Check to see if game has been won among regular boards and transposed boards:
+        function checkBoardsAgainstNumbersDrawn(arrayOfBoards, numbersDrawnSoFar) {
+            if(lookupBingoBoards(arrayOfBoards, numbersDrawnSoFar).some(board => board === true)) {
+                gameOver = true;
+                const winningIndex = lookupBingoBoards(arrayOfBoards, numbersDrawnSoFar).findIndex(element => element === true);
+                const scoreArray = arrayOfBoards[winningIndex].map(row => row.filter(el => !numbersDrawnSoFar.includes(el))).flat();
+
+                //Calculate final score:
+                const score = scoreArray.reduce((accumulator, val) => accumulator + val) * numbersDrawnSoFar[numbersDrawnSoFar.length - 1];
+                console.log("Puzzle 1: Game has been won! Score: ", score);
+            }
+        }
+        checkBoardsAgainstNumbersDrawn(bingoBoards, drawnNumbers)
+        checkBoardsAgainstNumbersDrawn(transposedBingoBoards, drawnNumbers)
+    }
+
 }
 
-
-
-
+puzzle1CalculateScore(arrays);
