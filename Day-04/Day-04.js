9f0c8e5b6ca2348e91e70d007bd4e7050f5a514f
Rohit Bhat <rohitwritescode@gmail.com>
2021-12-06 20:32:58 -0800

    Completed puzzle part 2 for Day 4

--- Day-04/Day-04.js
+++ Day-04/Day-04.js
@@ -1,4 +1,4 @@
-//Read input and organize into required arrays:
+// Read input and organize into required arrays:
 const fs = require("fs");
 const input = fs.readFileSync("./input.txt", "utf-8");
 const arrays = input.split('\r\n\r\n').map((element, idx) => {
@@ -6,9 +6,12 @@ const arrays = input.split('\r\n\r\n').map((element, idx) => {
     else return element
 });
 
-// Puzzle 1: Calculate score for first game that is won:
-function puzzle1CalculateScore(inputArrays) {
+// Puzzles 1 and 2: Calculate score for first game and last game that is won:
+function solvePuzzles(inputArrays) {
+    // Select numbers to draw and move into an array:
     const numbersToDraw = inputArrays.shift().split(',').map(el => parseInt(el));
+
+    // Select and transpose bingo boards into arrays:
     const bingoBoards = inputArrays.map(array => array.map(element => element.toString().split(' ').filter(cleanedEl => cleanedEl !== '').map(num => parseInt(num))));
     const transposedBingoBoards = bingoBoards.map(board => transpose(board));
 
@@ -24,33 +27,76 @@ function puzzle1CalculateScore(inputArrays) {
 
     // Check array of boards with numbers that have been drawn so far:
     function lookupBingoBoards(bingoBoardsArray, numbersToLookup) {
-        let checker = (arr, target) => target.every(v => arr.includes(v));
+        const checker = (arr, target) => target.every(v => arr.includes(v));
         const checkedBingoBoardsArray = bingoBoardsArray.map(board => board.map(row => checker(numbersToLookup,row)).some(element => element === true));
         return checkedBingoBoardsArray;
     }
 
     // Begin drawing numbers one by one, and verify drawn numbers with all boards:
     let drawnNumbers =[];
-    let gameOver = false;
-    while(gameOver !== true) {
+    let firstBoardWon = false;
+    let lastBoardWon = false;
+    let indexOfLastBoard;
+    let gameStateInitArray = new Array(bingoBoards.length).fill(false);
+    let gameStateTrueIndices = new Set();
+    let gameState = [];
+    while(!firstBoardWon || !lastBoardWon) {    
         drawnNumbers.push(drawNumber(numbersToDraw));
 
-        // Check to see if game has been won among regular boards and transposed boards:
-        function checkBoardsAgainstNumbersDrawn(arrayOfBoards, numbersDrawnSoFar) {
-            if(lookupBingoBoards(arrayOfBoards, numbersDrawnSoFar).some(board => board === true)) {
-                gameOver = true;
-                const winningIndex = lookupBingoBoards(arrayOfBoards, numbersDrawnSoFar).findIndex(element => element === true);
+        // Puzzle Part 1: Find first board that wins the game:
+        function findFirstBoardWon(currentState, arrayOfBoards, numbersDrawnSoFar) {
+            if(currentState.some(board => board === true)) {
+                firstBoardWon = true;
+                const winningIndex = currentState.findIndex(element => element === true);
                 const scoreArray = arrayOfBoards[winningIndex].map(row => row.filter(el => !numbersDrawnSoFar.includes(el))).flat();
 
-                //Calculate final score:
+                // Calculate final score:
                 const score = scoreArray.reduce((accumulator, val) => accumulator + val) * numbersDrawnSoFar[numbersDrawnSoFar.length - 1];
-                console.log("Puzzle 1: Game has been won! Score: ", score);
+                console.log("Puzzle 1: First board has been won! Score: ", score);
+            }
+        }    
+        
+        // Puzzle Part 2: Find last board that wins the game:
+        function findLastBoardWon(currentState, arrayOfBoards, numbersDrawnSoFar) {
+            if(currentState.every(board => board === true)) {
+                lastBoardWon = true;
+                indexOfLastBoard = [...gameStateTrueIndices][gameStateTrueIndices.size - 1];
+                const scoreArray = arrayOfBoards[indexOfLastBoard].map(row => row.filter(el => !numbersDrawnSoFar.includes(el))).flat();
+
+                // Calculate final score:
+                const score = scoreArray.reduce((accumulator, val) => accumulator + val) * numbersDrawnSoFar[numbersDrawnSoFar.length - 1];
+                console.log("Puzzle 2: Last board has been won! Score: ", score);
             }
         }
-        checkBoardsAgainstNumbersDrawn(bingoBoards, drawnNumbers)
-        checkBoardsAgainstNumbersDrawn(transposedBingoBoards, drawnNumbers)
+        
+        // Find all boards that have won so far, and boards that are yet to win:
+        function getLatestGameState(boards, transposedBoards, numbersDrawnSoFar) {
+            lookupBingoBoards(boards, numbersDrawnSoFar).map((board, idx) => {
+                if(board === true) {
+                    gameStateTrueIndices.add(idx);
+                }
+            });
+            lookupBingoBoards(transposedBoards, numbersDrawnSoFar).map((board, idx) => {
+                if(board === true) {
+                    gameStateTrueIndices.add(idx);
+                }
+            });
+            return gameStateInitArray.map((board, idx) => {
+                if (gameStateTrueIndices.has(idx)) {
+                    board = true;
+                } 
+                return board;
+            })
+        }
+        
+        // Get the latest state of the game:
+        gameState = [...getLatestGameState(bingoBoards,transposedBingoBoards,drawnNumbers)];
+        if (!firstBoardWon) {
+            findFirstBoardWon(gameState, bingoBoards, drawnNumbers);
+        } else {
+            findLastBoardWon(gameState, bingoBoards, drawnNumbers);
+        }
     }
-
 }
 
-puzzle1CalculateScore(arrays);
+solvePuzzles(arrays);
